c8edb171d269aa95419e399759c1ed275463b9a4
luyi <2913911073@qq.com>
2024-12-24 16:01:24 +0800

    flame--点击移动效果

--- project/flame_test/lib/MoveComponent.dart
+++ project/flame_test/lib/MoveComponent.dart
@@ -2,19 +2,50 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/layout.dart';
 import 'package:flame_test/Direction.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class MoveComponent extends RectangleComponent {
   Vector2? parentSize;
   Direction? currentDir;
   ///move rection
-  double length=2;
+  double length=1;
+  ///destination
+  Vector2? destination;
+
+MoveComponent({
+    super.position,
+    super.size,
+    super.scale,
+    super.angle,
+    super.anchor,
+    super.children,
+    super.priority,
+    super.paint,
+    super.paintLayers,
+    super.key,
+  });
 
 
   @override
   FutureOr<void> onLoad() {
     // TODO: implement onLoad
     size = Vector2(50, 50);
+    
+      children.add(
+     AlignComponent(
+    alignment: Anchor.center,
+    child: TextComponent(text: '人',textRenderer: TextPaint(
+    style: const TextStyle(
+      color: Colors.white,
+      fontSize: 50
+    )
+  ))
+  )
+    );
+    paint.color=Colors.transparent;
     return super.onLoad();
   }
 
@@ -22,9 +53,44 @@ class MoveComponent extends RectangleComponent {
   void update(double dt) {
     // TODO: implement update
     super.update(dt);
-    // 获取父组件的尺寸
+ destinationMove();
+
+  }
+  ///automatic move to destination
+  destinationMove(){
     if (parentSize == null && parent != null) {
-      parentSize = (parent! as FlameGame).size; // 确保 parent 不为 null
+      parentSize = (parent! as RectangleComponent).size; // 确保 parent 不为 null
+    }
+    if(destination==null){
+      return ;
+    }
+    if(position.x==destination!.x&&position.y==destination!.y){
+      destination=null;
+    }
+    if(position.x<destination!.x){
+      
+      moveRight();
+    }else if(position.x>destination!.x){
+      moveLeft();
+    }
+
+    if(position.y<destination!.y){
+      moveDown();
+    }else if(position.y>destination!.y){
+      moveUp();
+    }
+
+
+
+  }
+
+
+
+  ///according to direction to move
+  directionMove(){
+   // 获取父组件的尺寸
+    if (parentSize == null && parent != null) {
+      parentSize = (parent! as RectangleComponent).size; // 确保 parent 不为 null
     }
     if (parentSize != null) {
       switch(currentDir){
@@ -50,7 +116,8 @@ class MoveComponent extends RectangleComponent {
           break;
       }
     }
-  }
+  } 
+
 
   moveRight() {
     if (parentSize == null) {
--- project/flame_test/lib/demo.dart
+++ project/flame_test/lib/demo.dart
@@ -1,76 +1,82 @@
+import 'dart:async';
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 
 import 'package:flame_test/ButtonComponent.dart';
 import 'package:flame_test/Direction.dart';
 import 'package:flame_test/MoveComponent.dart';
+import 'package:flutter/material.dart';
 
 
 class MyGame extends FlameGame {
-  late final ButtonComponent1 up;
-  late final ButtonComponent1 left;
-  late final ButtonComponent1 right;
-  late final ButtonComponent1 down;
 
+late final BackGroundLayer b;
   ///button size
   final btsize = Vector2(50, 50);
 
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    QController().square=MoveComponent();
-      // QController().square.size=Vector2(50,50);
+            QController().square=MoveComponent();
+      QController().square.size=Vector2(50,50);
       QController().square.position=Vector2(100,100);
-    
-    add(QController().square as Component);
+    //   children.add(QController().square as Component);
+    // add(QController().square as Component);
     QController().content = this;
-    // button
-    //   ..position = Vector2(100, 100)
-    //   ..size = Vector2(200, 60);
-    up = ButtonComponent1(direction: Direction.up)
-      ..size = btsize
-      ..position = Vector2(size.x / 2 - 25, size.y - 170);
-    left = ButtonComponent1(direction: Direction.left)
-      ..size = btsize
-      ..position = Vector2(size.x / 2 - 75, size.y - 120);
-    right = ButtonComponent1(direction: Direction.right)
-      ..size = btsize
-      ..position = Vector2(size.x / 2 + 25, size.y - 120);
-    down = ButtonComponent1(direction: Direction.down)
-      ..size = btsize
-      ..position = Vector2(size.x / 2 - 25, size.y - 70);
-    add(up);
-    add(left);
-    add(right);
-    add(down);
+    var backGroundLayer = BackGroundLayer(size: size,paint: Paint()..color=Colors.transparent,children: [QController().square]);
+    add(backGroundLayer);
+
+
+
   }
 
-  // @override
-  // void update(double dt) {
-  //   super.update(dt);
-
-  //   // Move the square to the right every frame
-  //   controller.square?.position.x += 100 * dt;
-  //   if (controller.square!.position.x > size.x) {
-  //     controller.square?.position.x = -50;
-  //   }
-  // }
-
-  // @override
-  // void render(Canvas canvas) {
-  //   super.render(canvas);
-  //   // Draw the square manually using the custom canvas drawing
-  //   final paint = Paint()..color = Colors.blue;
-  //   canvas.drawRect(
-  //     Rect.fromLTWH(
-  //         controller.square!.position.x,
-  //         controller.square!.position.y,
-  //         controller.square!.size.x,
-  //         controller.square!.size.y),
-  //     paint,
-  //   );
-  // }
 }
+
+class BackGroundLayer extends RectangleComponent with TapCallbacks{
+  
+  BackGroundLayer({
+    super.position,
+    super.size,
+    super.scale,
+    super.angle,
+    super.anchor,
+    super.children,
+    super.priority,
+    super.paint,
+    super.paintLayers,
+    super.key,
+  });
+  
+  // @override
+  // // TODO: implement paint
+  // Paint get paint => (){
+  //      final Paint _paint = Paint()..color=Colors.red;
+   
+  //   return _paint;
+  // }();
+@override
+  FutureOr<void> onLoad() {
+    // TODO: implement onLoad
+
+    return super.onLoad();
+
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    // TODO: implement onTapDown
+    super.onTapDown(event);
+    
+    QController().square.destination=event.localPosition;
+
+  }
+
+}
+
 
 class QController {
   // 私有构造函数
